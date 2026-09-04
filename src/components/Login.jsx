dcfe182e5dd9713aa968bee9c2825d93c2c06ad2
Sayali <bhutkarsayali@gmail.com>
2026-09-04 15:33:15 +0530

    Login and profile routes and API calls

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,7 +23,9 @@ const Login = () => {
       );
 
       console.log(res.data);
+      
       dispatch(addUser(res.data));
+
       return navigate("/");
     } catch (err) {
       console.error(err.message);
